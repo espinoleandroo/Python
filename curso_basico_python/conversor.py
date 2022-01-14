94e55ead11cdef36604e1382d0c69f0ce6c62fa8
espinoleandroo <espinoleandroo@gmail.com>
2022-01-13 22:28:08 -0600

    Modularizando nuestro conversor de monedas

--- curso_basico_python/conversor.py
+++ curso_basico_python/conversor.py
@@ -1,3 +1,11 @@
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("¿Cuandos pesos " + tipo_pesos + " tienes? ")
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+
 menu = """
 Bienvenido al conversor de monedas 💰
 
@@ -11,29 +19,11 @@ Elige la opción:
 opcion = int(input(menu))
 
 if opcion == 1:
-    pesos = input("¿Cuandos pesos mexicanos tienes? ")
-    pesos = float(pesos)
-    valor_dolar = 20.35
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dólares")
+    conversor("mexicanos", 20.35)
 elif opcion == 2:
-    pesos = input("¿Cuandos pesos argentinos tienes? ")
-    pesos = float(pesos)
-    valor_dolar = 65
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dólares")
+    conversor("argentinos", 65)
 elif opcion == 3:
-    pesos = input("¿Cuandos pesos colombianos tienes? ")
-    pesos = float(pesos)
-    valor_dolar = 3875
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dólares")
+    conversor("colombianos", 3875)
 else:
     print("Ingresa una opción correcta por favor")
 
