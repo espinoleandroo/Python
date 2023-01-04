68a2c3ec22f925dc32d38da35bbd9330b20f865b
Leandro Espino <espinoleandroo@gmail.com>
2023-01-03 21:52:51 -0600

    Tipos de datos

---
+++ curso_fundamentos_python/03_types.py
@@ -0,0 +1,19 @@
+# String
+my_name = "Leandro"
+print(my_name)
+print(type(my_name))
+
+# int
+my_age = 26
+print(my_age)
+print(type(my_age))
+
+# boolean
+is_single = True
+print(is_single)
+print(type(is_single))
+
+# inputs
+my_age = input("¿Cual es tu edad?")
+print(my_age)
+print(type(my_age))
