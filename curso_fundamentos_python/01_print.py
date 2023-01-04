3f9872256429b04ebd5042c3e1fb3540b9c10724
Leandro Espino <espinoleandroo@gmail.com>
2023-01-03 21:43:08 -0600

    Variables

---
+++ curso_fundamentos_python/01_print.py
@@ -0,0 +1,19 @@
+print("Hola esto es el archivo 01")
+
+print("Hola Soy Leandro y tengo 26 años")
+
+# Comentario entre lineas
+
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+# Esto es un comentario
+
+'''
+Comentario
+de 
+varias
+lineas
+'''
---
+++ curso_fundamentos_python/02_vars.py
@@ -0,0 +1,16 @@
+print("Hola vars")
+
+my_name = "Leandro"
+print(my_name)
+
+my_age = 26
+print(my_age)
+
+my_name = "Leandro Espino"
+print(my_name)
+
+
+my_name = input("¿Cual es tu nombre?")
+print(my_name)
+my_age = input("¿Cual es tu edad?")
+print(my_age)
