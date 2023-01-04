de126d8fb73c948ecfd49a77f099f43c913c15bc
Leandro Espino <espinoleandroo@gmail.com>
2023-01-03 22:09:23 -0600

    Numbers

---
+++ curso_fundamentos_python/05_numbers.py
@@ -0,0 +1,30 @@
+lives = 3
+print(type(lives))
+age = 26
+buget = 100
+
+temperature = 12.12
+print(type(temperature))
+
+lives = 2
+print(lives)
+lives = 1
+print(lives)
+
+lives = 12 + 15
+print(lives)
+
+lives = lives - 1
+print(lives)
+
+lives -= 1
+print(lives)
+
+lives += 5
+print(lives)
+
+number = 450000000000000000000000000000.1
+print(number)
+
+number = 0.00000000000000000000000000045
+print(number)
