2cd5e6164ec3ce4aff827ebe79374bb295171245
Leandro Espino <espinoleandroo@gmail.com>
2023-01-03 22:19:30 -0600

    Transformación de tipos

---
+++ curso_fundamentos_python/07_transformation.py
@@ -0,0 +1,15 @@
+name = "Leandro"
+print(type(name))
+name = 11
+print(type(name))
+name = True
+print(type(name))
+
+age = 26
+print("Mi edad es " + str(age))
+
+age = input("Escribe tu edad => ")
+print(age)
+age = int(age)
+age += 10
+print("Tu edad en 10 años será {age}")
