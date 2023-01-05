b9a0bd845c344b7b065256a77a266baf8edfdc59
Leandro Espino <espinoleandroo@gmail.com>
2023-01-04 22:26:38 -0600

    Operadores lógicos: and y or

---
+++ curso_fundamentos_python/11_logic.py
@@ -0,0 +1,15 @@
+print("* " * 10)
+
+print("AND")
+print("True  and True  => ", True and True)
+print("True  and False => ", True and False)
+print("False and True  => ", False and True)
+print("False and False => ", False and False)
+
+print("* " * 10)
+
+print("OR")
+print("True  or True  => ", True or True)
+print("True  or False => ", True or False)
+print("False or True  => ", False or True)
+print("False or False => ", False or False)
