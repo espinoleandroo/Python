c49a368cd9e911124f0700fc893558f8c11346fe
Leandro Espino <espinoleandroo@gmail.com>
2023-01-06 22:32:43 -0600

    Proyecto: aplicando ciclos

--- curso_fundamentos_python/main.py
+++ curso_fundamentos_python/main.py
@@ -2,11 +2,25 @@ import random
 
 options = ("piedra", "papel", "tijera")
 
-user_option = input("piedra, papel o tijera => ")
+round = 1
+user_wins = 0
+computer_wins = 0
 
-if not user_option in options:
-    print("Opcion invalida")
-else :
+while True:
+
+    print("* " * 10 )
+    print("Round ", round )
+    print("* " * 10 )
+    print("computer", computer_wins )
+    print("user", user_wins )
+    print("* " * 10 )
+
+    user_option = input("piedra, papel o tijera => ")
+    round += 1
+    
+    if not user_option in options:
+        print("Opcion invalida")
+        continue
     user_option = user_option.lower()
     computer_option = random.choice(options)
 
@@ -18,15 +32,30 @@ else :
     elif user_option == "piedra":
         if computer_option == "tijera":
             print("User gano!")
+            user_wins += 1
         else:
             print("Computer gano!")
+            computer_wins += 1
     elif user_option == "papel":
         if computer_option == "piedra":
             print("User gano!")
+            user_wins += 1
         else:
             print("Computer gano!")
+            computer_wins += 1
     elif user_option == "tijera":
         if computer_option == "papel":
             print("User gano!")
+            user_wins += 1
         else:
             print("Computer gano!")
+            computer_wins += 1
+    
+    if computer_wins == 2:
+        print("El ganador es la computadora")
+        break
+    if user_wins == 2:
+        print("El ganador es el usuario")
+        break
+
+    round += 1
