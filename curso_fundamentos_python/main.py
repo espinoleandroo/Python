cff580bf721233bb2226c9fa4dccb2c6696008b6
Leandro Espino <espinoleandroo@gmail.com>
2023-01-06 21:46:30 -0600

    Proyecto: tuplas

--- curso_fundamentos_python/main.py
+++ curso_fundamentos_python/main.py
@@ -1,21 +1,32 @@
-user_option = input("piedra, papel o tijera => ")
-user_option = user_option.lower()
-computer_option = "tijera"
+import random
 
-if user_option == computer_option:
-    print("Empate!")
-elif user_option == "piedra":
-    if computer_option == "tijera":
-        print("User gano!")
-    else:
-        print("Computer gano!")
-elif user_option == "papel":
-    if computer_option == "piedra":
-        print("User gano!")
-    else:
-        print("Computer gano!")
-elif user_option == "tijera":
-    if computer_option == "papel":
-        print("User gano!")
-    else:
-        print("Computer gano!")
+options = ("piedra", "papel", "tijera")
+
+user_option = input("piedra, papel o tijera => ")
+
+if not user_option in options:
+    print("Opcion invalida")
+else :
+    user_option = user_option.lower()
+    computer_option = random.choice(options)
+
+    print("User option => ", user_option)
+    print("Computer option => ", computer_option)
+
+    if user_option == computer_option:
+        print("Empate!")
+    elif user_option == "piedra":
+        if computer_option == "tijera":
+            print("User gano!")
+        else:
+            print("Computer gano!")
+    elif user_option == "papel":
+        if computer_option == "piedra":
+            print("User gano!")
+        else:
+            print("Computer gano!")
+    elif user_option == "tijera":
+        if computer_option == "papel":
+            print("User gano!")
+        else:
+            print("Computer gano!")
