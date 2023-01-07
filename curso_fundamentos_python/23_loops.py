7d890e10da6365784e9a2b267ab2bc2ab4c340f8
Leandro Espino <espinoleandroo@gmail.com>
2023-01-06 22:23:18 -0600

    Ciclos anidados

---
+++ curso_fundamentos_python/23_loops.py
@@ -0,0 +1,14 @@
+matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+print(matriz[0])
+print(matriz[0][1])
+print(matriz[2][2])
+
+for row in matriz:
+    print(row)
+    for column in row:
+        print(column)
